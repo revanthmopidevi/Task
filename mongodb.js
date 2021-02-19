c3971797e4685fbd39525f6e423d1b0a2c5f63c7
Revanth Mopidevi <revanthmopidevi@gmail.com>
2021-02-19 15:28:27 +0530

    collections

--- mongodb.js
+++ mongodb.js
@@ -4,8 +4,50 @@ const MongoClient = mongodb.MongoClient
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-app"
 
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
+MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: true}, (error, client) => {
     if (error) {
-        console.log(error)
+        return console.log("Unable to connect to database.")
     }
+    const db = client.db(databaseName)
+    
+    // db.collection('users').insertOne({
+    //     name: 'Revanth',
+    //     age: 21
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to insert user.")
+    //     }
+        
+    //     console.log(result.ops)
+    // })
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Revanth',
+    //         age: 21
+    //     }, {
+    //         name: "Mopidevi",
+    //         age: 21
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log("Unable to insert documents.")
+    //     }
+
+    //     console.log(result.ops)
+    // })
+    db.collection('tasks').insertMany([
+        {
+            description: "",
+            completed: false
+        }, {
+            description: "Write",
+            completed: false
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log("Unable to insert tasks.")
+        }
+
+        console.log(result.ops)
+    })
 })
