9081e92df13f5f0dae19b97996368b147de2afe5
Revanth Mopidevi <revanthmopidevi@gmail.com>
2021-02-23 11:30:08 +0530

    mongod

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,8 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+// const mongodb = require('mongodb')
+// const MongoClient = mongodb.MongoClient
+// const ObjectID = mongodb.ObjectID
+
+const {MongoClient, ObjectID} = require('mongodb')
 
 const connectionURL = "mongodb://127.0.0.1:27017"
 const databaseName = "task-app"
@@ -9,45 +12,29 @@ MongoClient.connect(connectionURL, {useNewUrlParser: true, useUnifiedTopology: t
         return console.log("Unable to connect to database.")
     }
     const db = client.db(databaseName)
-    
-    // db.collection('users').insertOne({
-    //     name: 'Revanth',
-    //     age: 21
-    // }, (error, result) => {
-    //     if (error) {
-    //         return console.log("Unable to insert user.")
-    //     }
-        
-    //     console.log(result.ops)
-    // })
-    // db.collection('users').insertMany([
+
+    // db.collection('tasks').insertMany([
     //     {
-    //         name: 'Revanth',
-    //         age: 21
+    //         description: "",
+    //         completed: false
     //     }, {
-    //         name: "Mopidevi",
-    //         age: 21
+    //         description: "Write",
+    //         completed: false
     //     }
     // ], (error, result) => {
     //     if (error) {
-    //         return console.log("Unable to insert documents.")
+    //         return console.log("Unable to insert tasks.")
     //     }
 
     //     console.log(result.ops)
     // })
-    db.collection('tasks').insertMany([
-        {
-            description: "",
-            completed: false
-        }, {
-            description: "Write",
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log("Unable to insert tasks.")
-        }
 
-        console.log(result.ops)
+    db.collection('tasks').findOne({
+        description: "Write"
+    }, (error, task) => {
+        if (error) {
+            return console.log("Unable to fetch object.")
+        }
+        console.log(task)
     })
 })
