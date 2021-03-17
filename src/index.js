e66ee7d6d854fbda6441274a07958000723279b1
Revanth <revanthmopidevi@gmail.com>
2021-03-17 16:32:07 +0530

    GET task

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((error) => {
         res.status(400).send(error)
     })
@@ -24,9 +24,29 @@ app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
     
     task.save().then(() => {
-        res.send(task)
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
+    })
+})
+
+app.get('/tasks/', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((error) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((error) => {
+        res.status(500).send()
     })
 })
 
