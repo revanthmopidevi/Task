4d8ce411939e5d8e44cd0dd21bc05804f559c75f
Revanth <revanthmopidevi@gmail.com>
2021-03-19 10:00:43 +0530

    Resource Update Endpoints

--- src/index.js
+++ src/index.js
@@ -41,6 +41,26 @@ app.get('/users/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+// Update user by id
+app.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    
+    if (!isValidOperation) {
+        return res.status(400).send({"error": "Invalid update operations."})
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
 
 /* Tasks */
 // Add task
@@ -75,6 +95,29 @@ app.get('/tasks/:id', async (req, res) => {
         res.status(500).send()
     }
 })
+// Update task by id
+app.patch('tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'status']
+    const isValidOperation = updates.every((update) => {
+        return allowedUpdates.includes(update)
+    })
+
+    if (!isValidOperation) {
+        return res.status(400).send({"error": "Invalid update operation."})
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+
+        if (!task) {
+            return res.status(404).send()
+        }
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
