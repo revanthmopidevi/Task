619451d8bc1269e7c23cfb0fc72877fd127a9fd4
Revanth Mopidevi <revanthmopidevi@.com>
2021-05-10 13:42:27 +0530

    connector URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://taskadmin:L%40wsofP0wer@cluster0-shard-00-00.qbqwi.mongodb.net:27017,cluster0-shard-00-01.qbqwi.mongodb.net:27017,cluster0-shard-00-02.qbqwi.mongodb.net:27017/task-database?ssl=true&replicaSet=atlas-116hgr-shard-0&authSource=admin&retryWrites=true&w=majority', {
+const mongo_url = 'mongodb://taskadmin:L%40wsofP0wer@cluster0-shard-00-00.qbqwi.mongodb.net:27017,cluster0-shard-00-01.qbqwi.mongodb.net:27017,cluster0-shard-00-02.qbqwi.mongodb.net:27017/task-database?ssl=true&replicaSet=atlas-116hgr-shard-0&authSource=admin&retryWrites=true&w=majority'
+
+mongoose.connect(mongo_url, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
